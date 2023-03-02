bba45c17b534132bdf213213298873fffea6187b
connoringold <connoringold@gmail.com>
2023-03-02 23:27:00 +0000

    work on about me

--- components/ImageText.tsx
+++ components/ImageText.tsx
@@ -6,7 +6,7 @@ const ImageText = () => {
 	return (
 		<div className="flex flex-col items-center justify-center gap-10 py-5 border-t lg:py-10 md:flex-row md:justify-between">
 			<div className="md:w-2/3">
-				<Image className="w-full rounded-2xl" src="/images/banner.webp " width={500} height={500} alt="hero image" />
+				<Image className="w-full rounded-2xl" src="/images/banner.webp" width={500} height={500} alt="hero image" />
 			</div>
 			<div className="md:w-1/3">
 				<h2 className="mb-5 text-4xl font-bold text-center md:text-left">
