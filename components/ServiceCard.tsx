f6043013d65e2333a329676bda540e7b3c70aa09
connoringold <connoringold@gmail.com>
2023-11-02 21:05:09 +0000

    mobile fixes

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -18,8 +18,8 @@ const ServiceCard = (props: Props): JSX.Element => {
 	return (
 		// if align is left, then add the class 'flex-row-reverse' to the section
 		<section className={`flex my-5 py-5 gap-10 border-b flex-wrap lg:flex-nowrap ${props.align === 'right' && 'flex-row-reverse'}`}>
-			<div className="flex-1 pr-10 border-r">{props.children}</div>
-			<div className="flex flex-col justify-center flex-1 gap-5 text-left">
+			<div className="flex-1 border-b lg:pr-10 lg:border-b-0 lg:border-r">{props.children}</div>
+			<div className="flex flex-col justify-center gap-5 text-left lg:flex-1">
 				<h5>{props.aboveHeadingText}</h5>
 				<h3 className="text-2xl font-normal lg:text-4xl">{props.title}</h3>
 				<h5>{props.date}</h5>
